884d7ffbfcf1d0c2867b92ef6ea926b765a29907
Benjamin <bstanf@uw.edu>
2022-10-24 21:08:37 -0700

    built out the reactionsSchema in my thought.js model

--- models/thought.js
+++ models/thought.js
@@ -1,7 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 
 const reactionsSchema = new Schema(
-
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) = dateFromat(createdAtVal),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
 );
 
 const thoughtSchema = new Schema(
